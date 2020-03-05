2d04f919cf9e0e9e2290067c399c0370714d4eed
Shpakova Alena <shpakovadev@gmail.com>
2020-03-05 13:14:16 +0300

    section header

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,34 @@
 import React from "react"
 
 const MainPage = () => {
-  return <div className="header"></div>
+  return (
+    <div className="main-page">
+      <section className="header">
+        <div className="container">
+          <div className="header__inner">
+            <span className="header__logo">VoGo</span>
+            <nav className="header__nav nav">
+              <a href="#0" className="nav__link">
+                About
+              </a>
+              <a href="#0" className="nav__link">
+                Service
+              </a>
+              <a href="#0" className="nav__link">
+                Blog
+              </a>
+              <a href="#0" className="nav__link">
+                Work
+              </a>
+              <a href="#0" className="nav__link">
+                Contact
+              </a>
+            </nav>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default MainPage
